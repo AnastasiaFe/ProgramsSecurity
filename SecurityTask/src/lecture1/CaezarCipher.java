90cc10a32140f382adc43a57bd3d1d4aaee67c1c
Anastasia Fedorenko <nastia.federenko@gmail.com>
2017-02-19 20:42:51 +0200

    ceazar cipher updated

--- SecurityTask/src/lecture1/CaezarCipher.java
+++ SecurityTask/src/lecture1/CaezarCipher.java
@@ -5,7 +5,6 @@ public class CaezarCipher {
 
     private static final String TEXT3_PATH = "text3.txt";
     private static final int OFFSET = 12;
-    private static final int ALPHABET_CAPACITY = 26;
 
     public static void main(String[] args) {
 
@@ -14,24 +13,17 @@ public class CaezarCipher {
         System.out.println(decode(encode(str, OFFSET), OFFSET));
     }
 
-    private static String decode(String enc, int offset) {
-        return encode(enc, ALPHABET_CAPACITY - offset);
+    private static String encode(String input, int key) {
+        StringBuilder sb = new StringBuilder(input.length());
+
+        for (char c : input.toCharArray()) {
+            sb.append((char)(c + key));
+        }
+
+        return sb.toString();
     }
 
-    private static String encode(String enc, int offset) {
-        offset = offset % ALPHABET_CAPACITY + ALPHABET_CAPACITY;
-        StringBuilder encoded = new StringBuilder();
-        for (char i : enc.toCharArray()) {
-            if (Character.isLetter(i)) {
-                if (Character.isUpperCase(i)) {
-                    encoded.append((char) ('A' + (i - 'A' + offset) % ALPHABET_CAPACITY));
-                } else {
-                    encoded.append((char) ('a' + (i - 'a' + offset) % ALPHABET_CAPACITY));
-                }
-            } else {
-                encoded.append(i);
-            }
-        }
-        return encoded.toString();
+    private static String decode(String input, int key) {
+        return encode(input, -key);
     }
 }
